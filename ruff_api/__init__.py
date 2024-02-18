9b4413236167d85b80b9fcdaeb20b964f41917c5
Amethyst Reese <amethyst@n7.gg>
2024-02-17 16:41:22 -0800

    Export author/version

--- ruff_api/__init__.py
+++ ruff_api/__init__.py
@@ -5,13 +5,14 @@
 Experimental Python API for Ruff
 """
 
-from .__version__ import __version__
-
 __author__ = "Amethyst Reese"
 
+from .__version__ import __version__
 from ._rust import format_string, FormatOptions
 
 __all__ = (
+    "__author__",
+    "__version__",
     "format_string",
     "FormatOptions",
 )
