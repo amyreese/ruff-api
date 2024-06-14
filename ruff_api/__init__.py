11c057e67ca9ff383c7bd3eb71270b9cbf5ff35f
Amethyst Reese <amethyst@n7.gg>
2024-06-14 12:42:16 -0700

    Move exceptions into native Python code
    This allows exceptions from ruff-api to be pickled.

--- ruff_api/__init__.py
+++ ruff_api/__init__.py
@@ -10,13 +10,18 @@ __author__ = "Amethyst Reese"
 from .__ruff_version__ import ruff_version
 from .__version__ import __version__
 from ._rust import format_string, FormatOptions, isort_string, SortOptions
+from .errors import FormatError, ParseError, PrintError, RuffError
 
 __all__ = (
     "__author__",
     "__version__",
     "format_string",
+    "FormatError",
     "FormatOptions",
     "isort_string",
+    "ParseError",
+    "PrintError",
     "SortOptions",
     "ruff_version",
+    "RuffError",
 )
